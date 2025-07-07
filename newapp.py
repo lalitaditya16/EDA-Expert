5192326dbf06fa40d400b089800650971ab5721b
Lalitaditya Charapanjeri <122503975+lalitaditya16@users.noreply.github.com>
2025-07-07 12:24:50 +0530

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -79,7 +79,7 @@ if input_text:
 
     with st.spinner("Cooking up some shit..."):
         relevant_docs = retriever.invoke(input_text)
-        response = retriever_chain.invoke({
+        response = chain.invoke({
             "context":relevant_docs,
             "question": input_text,
             "history": history
