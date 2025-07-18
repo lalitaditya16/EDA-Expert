5eab10a1a2a0c45032ac24e0e85d50ed7b79b0fb
Lalitaditya Charapanjeri <122503975+lalitaditya16@users.noreply.github.com>
2025-07-18 13:34:37 +0530

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -61,7 +61,6 @@ df = None
 
 
 if df is not None:
-    df=pd.read_csv(uploaded_files)
     df_summary = f"""This dataset has {df.shape[0]} rows and {df.shape[1]} columns.
 Column names: {list(df.columns)}
 Data types:\n{df.dtypes.to_string()}
